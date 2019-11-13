22b9ade0e746ef1aaecb271a0fb6b68578b0d90f
thunder775 <rahulmasih61@gmail.com>
2019-11-13 15:14:42 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -3,5 +3,22 @@
 // Create a function that returns the simplified version of a fraction.
 /// Examples
 //  simplify("4/6") ➞ "2/3"
+String simplify(String fraction) {
+  List numbers = fraction.split('/');
+//print(numbers);
+  List factors = [];
+  int x = int.parse(numbers[0]);
+  int y = int.parse(numbers[1]);
+  for (int i = 1; i < (x < y ? x : y)+1; i++) {
+    if (x % i == 0 && y % i == 0) {
+      factors.add(i);
+    }
+  }
+//  print(factors);
+  return '${(x ~/ factors[factors.length - 1])}/${(y ~/ factors[factors.length - 1])}';
+}
+
 main() {
+  String s = simplify('4/3');
+  print(s);
 }
---
+++ bin/test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+import 'main.dart';
+main(){
+  test('simple fraction', (){
+    expect(simplify('22/1'),  '22/1');
+  });
+}
